b649ea5693dc00595a6aec93c90d6ca99ba3fae3
merisahakyan <sahakyan_m@bk.ru>
2017-04-12 19:26:41 +0400

    Grid

---
+++ Grid/Grid/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid
+{
+    public class User
+    {
+        public string Name1 { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public bool Checked { get; set; }
+
+    }
+}
