f0f4087078f878ad7a1805559e0971ae9a883ad7
merisahakyan <sahakyan_m@bk.ru>
2017-04-12 22:11:59 +0400

    databinding

---
+++ DataBinding/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User user = new User { Firstname = "Meri", Lastname = "Sahakyan" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = user;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{user.Firstname}   {user.Lastname}");
+        }
+    }
+}
